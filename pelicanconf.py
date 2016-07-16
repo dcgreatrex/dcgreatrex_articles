da4778ef8557274be7577165567dd54d371cc1d5
dcgretarex <dcgreatrex@aol.com>
2016-07-16 23:57:11 +0100

    add changes

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,5 +31,33 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 10
 
+# This requires Pelican 3.3+
+STATIC_PATHS = ['images', 'pages', 'favicon.png']
+
+# Theme and plugins
+THEME = 'pelican-octopress-theme/'
+# PLUGIN_PATHS = ['pelican-plugins/']
+# PLUGINS = ['summary', 'liquid_tags.img', 'liquid_tags.video',
+#            'liquid_tags.include_code', 'liquid_tags.notebook',
+#            'liquid_tags.literal']
+
+#Github include settings
+GITHUB_USER = 'dcgreatrex'
+GITHUB_REPO_COUNT = 5
+GITHUB_SKIP_FORK = False
+GITHUB_SHOW_USER_LINK = False
+
+# RSS/Atom feeds
+FEED_DOMAIN = SITEURL
+FEED_ATOM = 'atom.xml'
+
+# Search
+SEARCH_BOX = True
+
+NEWEST_FIRST_ARCHIVES = False
+
+# Social media
+FACEBOOK_LIKE = True
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
